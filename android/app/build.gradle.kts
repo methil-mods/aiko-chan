88dd188a58fde37509ff3be7d58958d4c2a54a45
EthanCarollo <etcarollo@gmail.com>
2026-03-08 12:06:25 +0100

    WIP : work on splashscreen fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 dependencies {
     implementation(project(":data"))
     implementation(libs.androidx.core.ktx)
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
--- android/app/src/main/java/com/methil/aiko/MainActivity.kt
+++ android/app/src/main/java/com/methil/aiko/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.methil.aiko.ui.navigation.AikoNavigation
 import com.methil.aiko.ui.theme.AikoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
