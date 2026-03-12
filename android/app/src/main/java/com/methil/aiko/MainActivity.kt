2dd80671d2fd2a2dba945646ab7dfb6bf3e4e914
EthanCarollo <etcarollo@gmail.com>
2026-03-12 22:03:36 +0100

    Feat : add NFC scan on android app

--- android/app/src/main/java/com/methil/aiko/MainActivity.kt
+++ android/app/src/main/java/com/methil/aiko/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.methil.aiko
 
 import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -15,13 +20,51 @@ import com.methil.aiko.ui.navigation.AikoNavigation
 import com.methil.aiko.ui.theme.AikoTheme
 
 class MainActivity : ComponentActivity() {
+    private var nfcAdapter: NfcAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        
         setContent {
             AikoTheme {
                 AikoNavigation()
+            }
+        }
+        handleIntent(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intent = Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_MUTABLE)
+        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, null, null)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableForegroundDispatch(this)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
+    private fun handleIntent(intent: Intent) {
+        val action = intent.action
+        if (NfcAdapter.ACTION_TAG_DISCOVERED == action ||
+            NfcAdapter.ACTION_TECH_DISCOVERED == action ||
+            NfcAdapter.ACTION_NDEF_DISCOVERED == action
+        ) {
+            val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+            tag?.let {
+                val tagId = it.id.joinToString("") { byte -> "%02X".format(byte) }
+                Log.d("NFC_SCANNER", "NFC Tag Scanned: $tagId")
             }
         }
     }
